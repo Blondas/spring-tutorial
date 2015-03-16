aa931ce75912a11bfb086deed5e8f524b8a83400
blondas <n.krzysiek@gmail.com>
2015-03-16 22:59:37 +0000

    Lecure 25: The 'Autowired' Annotation

--- src/main/java/org/krisdrum/spring/test/tutorial/Logger.java
+++ src/main/java/org/krisdrum/spring/test/tutorial/Logger.java
@@ -1,17 +1,29 @@
 package org.krisdrum.spring.test.tutorial;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Logger {
+    @Autowired
     private ConsoleWriter consoleWriter;
+    @Autowired
     private FileWriter fileWriter;
 
-    public void setConsoleWriter(ConsoleWriter consoleWriter) {
-        this.consoleWriter = consoleWriter;
-    }
+//    @Autowired
+//    public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+//        this.consoleWriter = consoleWriter;
+//        this.fileWriter = fileWriter;
+//    }
 
-    public void setFileWriter(FileWriter fileWriter) {
-        this.fileWriter = fileWriter;
-    }
+//    //    @Autowired
+//    public void setConsoleWriter(ConsoleWriter consoleWriter) {
+//        this.consoleWriter = consoleWriter;
+//    }
+//
+////    @Autowired
+//    public void setFileWriter(FileWriter fileWriter) {
+//        this.fileWriter = fileWriter;
+//    }
 
     public void writeFile(String text) {
         fileWriter.write(text);
