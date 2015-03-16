028d1d0b7d013835933bb63718183a561a7ebd3a
blondas <n.krzysiek@gmail.com>
2015-03-16 23:04:08 +0000

    LEcture 26: Optional Beans

--- src/main/java/org/krisdrum/spring/test/tutorial/Logger.java
+++ src/main/java/org/krisdrum/spring/test/tutorial/Logger.java
@@ -4,32 +4,26 @@ package org.krisdrum.spring.test.tutorial;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Logger {
-    @Autowired
     private ConsoleWriter consoleWriter;
-    @Autowired
     private FileWriter fileWriter;
 
-//    @Autowired
-//    public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
-//        this.consoleWriter = consoleWriter;
-//        this.fileWriter = fileWriter;
-//    }
+    @Autowired(required = false)
+    public void setConsoleWriter(ConsoleWriter consoleWriter) {
+        this.consoleWriter = consoleWriter;
+    }
 
-//    //    @Autowired
-//    public void setConsoleWriter(ConsoleWriter consoleWriter) {
-//        this.consoleWriter = consoleWriter;
-//    }
-//
-////    @Autowired
-//    public void setFileWriter(FileWriter fileWriter) {
-//        this.fileWriter = fileWriter;
-//    }
+    @Autowired
+    public void setFileWriter(FileWriter fileWriter) {
+        this.fileWriter = fileWriter;
+    }
 
     public void writeFile(String text) {
         fileWriter.write(text);
     }
 
     public void writeConsole(String text) {
-        consoleWriter.write(text);
+        if (consoleWriter != null) {
+            consoleWriter.write(text);
+        }
     }
 }
